e04e85a135ec2eb193b22347d020194eb9c07af0
Sivanthi Niroshika <shivniro@gmail.com>
2021-10-18 22:19:18 +1030

    Code updates

--- cypress/integration/api-tests/pet/status-code-200.js
+++ cypress/integration/api-tests/pet/status-code-200.js
@@ -1,88 +1,88 @@
-describe('API scripts - Pet Module',function(){
+describe('API scripts - Pet Module', function () {
 
 
-beforeEach(function(){
+  beforeEach(function () {
 
-cy.fixture("pet/petUrls").then(function(dataPetsUrl){
+    cy.fixture("pet/petUrls").then(function (dataPetsUrl) {
 
-this.dataPetsUrl=dataPetsUrl
+      this.dataPetsUrl = dataPetsUrl
 
-})
+    })
 
-cy.fixture("pet/createPets_200").then(function(dataCreatePets_200){
+    cy.fixture("pet/createPets_200").then(function (dataCreatePets_200) {
 
-    this.dataCreatePets_200=dataCreatePets_200
-    
+      this.dataCreatePets_200 = dataCreatePets_200
+
     })
 
 
-})
+  })
 
 
-it('Verify 200 Response for Get Pets Request', function() {
-    
+  it('Verify 200 Response for Get Pets Request', function () {
+
     //send the get request
-    cy.request('GET',this.dataPetsUrl.findPetsUrl).then(
+    cy.request('GET', this.dataPetsUrl.findPetsUrl).then(
       (response) => {
-        
+
         //verify 200 response was received
         expect(response.status).to.eq(200)
-        expect(response.body[0]).to.have.property('id') 
-        expect(response.body[0].category).to.have.property('id') 
-        expect(response.body[0]).to.have.property('status','available') 
+        expect(response.body[0]).to.have.property('id')
+        expect(response.body[0].category).to.have.property('id')
+        expect(response.body[0]).to.have.property('status', 'available')
 
       }
     )
 
-    
-  })
-
-
-it('Verify 200 Response for Add New Pet Request', function(){
-
-  var postRequest ={
-
-        method : "POST",
-        url:this.dataPetsUrl.createPetsUrl,
-        body:this.dataCreatePets_200
-
-  }
-        cy.request(postRequest).then(
-            (response) => {
-              //verify 200 response was received
-              expect(response.status).to.eq(200),
-               //verify properties
-              expect(response.body).to.have.property('name','doggie') 
-              expect(response.body).to.have.property('status','available') 
-            }
-          )
-
 
   })
 
 
+  it('Verify 200 Response for Add New Pet Request', function () {
 
-  it('Verify 200 Response for Update a Pet Request', function(){
+    var postRequest = {
 
-    var postRequest ={
-  
-          method : "PUT",
-          url:this.dataPetsUrl.updatePetsUrl,
-          body:this.dataCreatePets_200
-  
+      method: "POST",
+      url: this.dataPetsUrl.createPetsUrl,
+      body: this.dataCreatePets_200
+
     }
-          cy.request(postRequest).then(
-              (response) => {
-                //verify 200 response was received
-                expect(response.status).to.eq(200),
-                 //verify properties
-                expect(response.body).to.have.property('name','doggie') 
-                expect(response.body).to.have.property('status','available') 
-              }
-            )
-  
-  
-    })
+    cy.request(postRequest).then(
+      (response) => {
+        //verify 200 response was received
+        expect(response.status).to.eq(200),
+          //verify properties
+          expect(response.body).to.have.property('name', 'doggie')
+        expect(response.body).to.have.property('status', 'available')
+      }
+    )
+
+
+  })
+
+
+
+  it('Verify 200 Response for Update a Pet Request', function () {
+
+    var postRequest = {
+
+      method: "PUT",
+      url: this.dataPetsUrl.updatePetsUrl,
+      body: this.dataCreatePets_200
+
+    }
+    cy.request(postRequest).then(
+      (response) => {
+        //verify 200 response was received
+        expect(response.status).to.eq(200),
+          //verify properties
+          expect(response.body).to.have.property('name', 'doggie')
+        expect(response.body).to.have.property('status', 'available')
+      }
+    )
+
+
+  })
 
 
 
--- cypress/integration/api-tests/store/status-code-200.js
+++ cypress/integration/api-tests/store/status-code-200.js
@@ -1,93 +1,123 @@
-describe('API scripts - Store Module',function(){
+describe('API scripts - Store Module', function () {
 
 
-    beforeEach(function(){
-    
-    cy.fixture("store/storeUrls").then(function(dataStoresUrl){
-    
-    this.dataStoresUrl=dataStoresUrl
-    
-  
-   
-    
+    beforeEach(function () {
+
+        cy.fixture("store/storeUrls").then(function (dataStoresUrl) {
+
+            this.dataStoresUrl = dataStoresUrl
+
+
+
+
+        })
+
+
+        cy.fixture("store/orderPetFromStore_200").then(function (dataOrderPetFromStore_200) {
+
+            this.dataOrderPetFromStore_200 = dataOrderPetFromStore_200
+
+        })
+
+
+
+
     })
-  
-  
-    cy.fixture("store/orderPetFromStore_200").then(function(dataOrderPetFromStore_200){
 
-      this.dataOrderPetFromStore_200=dataOrderPetFromStore_200
-      
-      })
-  
-  
-  
-  
-  
-  
-  
-  })
 
-    
-    
-    it('Verify 200 Response for Get Pet Inventories by status', function() {
-        
+
+    it('Verify 200 Response for Get Pet Inventories by status', function () {
+
         //send the get request
-        cy.request('GET',this.dataStoresUrl.getPetInventoriesByStatus).then(
-          (response) => {
-            
-            //verify 200 response was received
-            expect(response.status).to.eq(200)
-            //verify the response properties
-            expect(response.body).to.have.property('Busy') 
-            expect(response.body).to.have.property('Available') 
-            expect(response.body).to.have.property('sold') 
-            expect(response.body).to.have.property('free') 
-            expect(response.body).to.have.property('updated') 
+        cy.request('GET', this.dataStoresUrl.getPetInventoriesByStatus).then(
+            (response) => {
 
-        
-    
-          }
-        )
-    
-        
-      })
-
-
-      it('Verify 200 Response for Place an order for a pet', function() {
-        
-        var postRequest ={
-
-          method : "POST",
-          url:this.dataStoresUrl.orderPetFromStore,
-          body:this.dataOrderPetFromStore_200
-  
-    }
-          cy.request(postRequest).then(
-              (response) => {
                 //verify 200 response was received
-               expect(response.status).to.eq(200)
-                 //verify properties
-               expect(response.body).to.have.property('petId',0) 
-               expect(response.body).to.have.property('quantity',0) 
-               expect(response.body).to.have.property('status','placed') 
-               expect(response.body).to.have.property('complete',true) 
+                expect(response.status).to.eq(200)
+                //verify the response properties
+                expect(response.body).to.have.property('Busy')
+                expect(response.body).to.have.property('Available')
+                expect(response.body).to.have.property('sold')
+                expect(response.body).to.have.property('free')
+                expect(response.body).to.have.property('updated')
+
+
+
+            }
+        )
+
+
+    })
+
+
+    it('Verify 200 Response for Place an order for a pet', function () {
+
+        var postRequest = {
+
+            method: "POST",
+            url: this.dataStoresUrl.orderPetFromStore,
+            body: this.dataOrderPetFromStore_200
+
+        }
+        cy.request(postRequest).then(
+            (response) => {
+                //verify 200 response was received
+                expect(response.status).to.eq(200)
+                //verify properties
+                expect(response.body).to.have.property('petId', 0)
+                expect(response.body).to.have.property('quantity', 0)
+                expect(response.body).to.have.property('status', 'placed')
+                expect(response.body).to.have.property('complete', true)
 
 
 
 
-              }
-              )
-    
-    
-      })
-  
-  
-  
-  
-  
-  
-  
-  })
-  
-  
-  
+            }
+        )
+
+
+    })
+
+
+
+
+    it('Verify 200 Response for Get Purchase Order By Id', function () {
+
+        //send the get request
+        cy.request('GET', this.dataStoresUrl.getPurchaseOrderById).then(
+            (response) => {
+
+                //verify 200 response was received
+                expect(response.status).to.eq(200)
+                //verify the response properties
+                expect(response.body).to.have.property('id')
+                expect(response.body).to.have.property('petID')
+                expect(response.body).to.have.property('quantity')
+                expect(response.body).to.have.property('status')
+                expect(response.body).to.have.property('complete')
+
+
+
+
+
+            }
+        )
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
